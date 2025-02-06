f9769f383cb1cc801447d62d90178e8c6a6699b7
mnajwananhar <mnajwananhardej@gmail.com>
2025-02-06 19:01:25 +0700

    change menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -419,7 +419,7 @@
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                         d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
                                                 </svg>
-                                                <span>Produk</span>
+                                                <span>Product</span>
                                             </a>
                                             <a href="{{ route('orders.manage') }}"
                                                 class="flex items-center gap-2 px-4 py-2 text-sm text-gray-600 hover:bg-[#FFF5E6]">
@@ -428,7 +428,7 @@
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                         d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
                                                 </svg>
-                                                <span>Pesanan</span>
+                                                <span>Orders</span>
                                             </a>
                                         </div>
                                     </div>
